01376c0a0540e1d495fe3304c20485674ca2d6c5
Magzhan <codebuster@bk.ru>
2021-10-27 17:41:25 +0600

    Test husky

--- generate_app_version.js
+++ generate_app_version.js
@@ -3,6 +3,19 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const { forceStdinTty } = require('force-stdin-tty');
+
+let overwroteStdin = false;
+try {
+    overwroteStdin = forceStdinTty();
+} catch {
+    console.error('Please push your code in a terminal.');
+    process.exit(1);
+}
+
+if (overwroteStdin) {
+    process.stdin.destroy();
+}
 
 rl.question("What is your name ? ", function(name) {
     rl.question("Where do you live ? ", function(country) {
